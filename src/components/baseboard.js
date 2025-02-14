d24b1acb83e8039f8a9dd812378d8512f65f014e
Davi Dias <davifernandosousadias18@gmail.com>
2025-02-14 13:54:31 -0300

    fix: removed ./ from the path to the images

--- src/components/baseboard.js
+++ src/components/baseboard.js
@@ -3,19 +3,19 @@ export function BaseBoard () {
  <section id="logoSection">
         <ul class="logo-brands">
           <li>
-            <img src="./assets/images/logos/logo_versace.svg" alt="Logo Versace" />
+            <img src="assets/images/logos/logo_versace.svg" alt="Logo Versace" />
           </li>
           <li>
-            <img src="./assets/images/logos/logo_zara.svg"/>
+            <img src="assets/images/logos/logo_zara.svg"/>
           </li>
           <li>
-            <img src="./assets/images/logos/logo_gucci.svg"/>
+            <img src="assets/images/logos/logo_gucci.svg"/>
           </li>
           <li>
-            <img src="./assets/images/logos/logo_prada.svg" alt="Logo Prada" class="logo-down"/>
+            <img src="assets/images/logos/logo_prada.svg" alt="Logo Prada" class="logo-down"/>
           </li>
           <li>
-            <img src="./assets/images/logos/logo_calvin_klein.svg" alt="Logo Calvin Klein" class="logo-down"/>
+            <img src="assets/images/logos/logo_calvin_klein.svg" alt="Logo Calvin Klein" class="logo-down"/>
           </li>
         </ul>
   </section>
